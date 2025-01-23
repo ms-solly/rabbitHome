ddc07231cfded84e2e58bf6550a6d189ea224d06
aditi-jogdand <theworldofthoughtsyo@gmail.com>
2025-01-23 16:59:08 +0530

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/rabbit.png" sizes="any" />
+        <link rel="icon" href="/favicon/rabbit.png" sizes="any" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body className={cn(geistMono.className, "bg-[#1c1c1cf0] text-[#e4e4e4] min-h-screen flex flex-col")}>        
@@ -30,7 +30,7 @@ export default function RootLayout({
               <div className="absolute inset-0 bg-black opacity-50"></div>
               <img src="/bg.png" alt="Rabbit" className="object-cover w-full h-full opacity-20 blur-sm fixed" />
             </div>
-            <div className="relative text-[#ffffffde] drop-shadow-md">{children}</div>
+            <div className="relative text-[#ffffffde] drop-shadow-md pb-36">{children}</div>
           </main>
         </div>
         <StatusLine />
